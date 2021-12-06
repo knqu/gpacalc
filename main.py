f83691b3d032a9d93463c78fe40a9090f043cc42
Kevin Qu <kevinqu2542@gmail.com>
2021-12-06 07:09:23 -0500

    began implementation of export feature

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import csv
 from rich.console import Console
 from rich.theme import Theme
 from rich.table import Table
+from os import mkdir, listdir
+from os.path import exists
+from openpyxl import Workbook
 
 semantic = Theme({
     'prompt': '#10a4e8 bold',
@@ -11,7 +13,7 @@ semantic = Theme({
     'dim': '#404040'
 })
 
-console = Console(theme=semantic)
+console = Console(theme=semantic, highlight=False)
 
 def hr():
     console.print('')
@@ -20,55 +22,21 @@ def hr():
 
 record = []
 
-def calculateGPA(weight):
-    addGPA = 0
-    if weight == 'w': # weighted
-        for el in record:
-            addGPA += el['gpa']
-    elif weight == 'u': # unweighted
-        for el in record:
-            if el['grade'] == 'a':
-                addGPA += 4.0
-            elif el['grade'] == 'a-':
-                addGPA += 3.667
-            elif el['grade'] == 'b+':
-                addGPA += 3.333
-            elif el['grade'] == 'b':
-                addGPA += 3.0
-            elif el['grade'] == 'b-':
-                addGPA += 2.667
-            elif el['grade'] == 'c+':
-                addGPA += 2.333
-            elif el['grade'] == 'c':
-                addGPA += 2.0
-            elif el['grade'] == 'c-':
-                addGPA += 1.667
-            elif el['grade'] == 'd+':
-                addGPA += 1.333
-            elif el['grade'] == 'd':
-                addGPA += 1.0
-            elif el['grade'] == 'd-':
-                addGPA += 0.667
-            elif el['grade'] == 'f':
-                addGPA += 0.0
-    return round(addGPA / len(record), 3)
-
 def init():
     hr()
-    console.print('gpacalc', style='info')
-    console.print('Created by Kevin Qu', style='info')
-    console.input('\npress [ enter ] to begin > ')
+    console.print('GPACALC', style='info')
+    console.print('CREATED BY KEVIN QU', style='info')
     menu()
 
 def menu():
     hr()
-    console.print('What would you like to do?\n', style='prompt')
-    console.print('[ n ] create new record', style='prompt')
-    console.print('[ v ] view current record', style='prompt')
-    console.print('[ e ] edit current record', style='prompt')
-    console.print('[ c ] export record as .csv', style='prompt')
-    console.print('[ h ] help', style='prompt')
-    console.print('[ x ] exit', style='prompt')
+    console.print('What would you like to do?', style='prompt')
+    console.print('[ n ] create new record')
+    console.print('[ v ] view current record')
+    console.print('[ e ] edit current record')
+    console.print('[ s ] export record as spreadsheet')
+    console.print('[ h ] help')
+    console.print('[ x ] exit')
     inpt = console.input('\n> ')
 
     if inpt == 'n':
@@ -77,17 +45,17 @@ def menu():
         view()
     elif inpt == 'e':
         edit()
-    elif inpt == 'c':
+    elif inpt == 's':
         export()
     elif inpt == 'h':
         hr()
         console.print('[ c ] create new record - creates a blank record and prompts you for element values')
         console.print('[ v ] view current record - view current record')
         console.print('[ e ] edit current record - change elements of current record')
-        console.print('[ n ] export record as .csv - export current record as a .csv (spreadsheet) file')
+        console.print('[ s ] export record as spreadsheet - export current record as an excel (.xlsx) file')
         console.input('\npress [ enter ] to continue > ')
     elif inpt == 'x':
-        console.print('Thank you for using gpacalc. Exiting program.', style='info')
+        console.print('\nThank you for using gpacalc. Exiting program.', style='info')
         exit()
     else:
         console.print('\n[ERROR] Please enter a valid option.', style='error')
@@ -112,7 +80,7 @@ def new():
                     inpt_split[1] = inpt_split[1].lower()
                     inpt_split[2] = inpt_split[2].lower()
 
-                    if inpt_split[0] and len(inpt_split[0]) <= 24 and inpt_split[1] in ['a', 'a-', 'b+', 'b', 'b-', 'c+', 'c', 'c-', 'd+', 'd', 'd-', 'f'] and inpt_split[2] in ['r', 'p', 'f'] and len(inpt_split) <= 3:
+                    if inpt_split[0] and len(inpt_split[0]) <= 24 and inpt_split[0] not in record_classes() and inpt_split[1] in ['a', 'a-', 'b+', 'b', 'b-', 'c+', 'c', 'c-', 'd+', 'd', 'd-', 'f'] and inpt_split[2] in ['r', 'p', 'f'] and len(inpt_split) <= 3:
                         gpa = 0
                         if inpt_split[1] == 'a' and inpt_split[2] == 'f':
                             gpa = 5.0
@@ -200,18 +168,18 @@ def new():
 
     hr()
 
-    console.print('Would you like to view the tutorial?\n', style='prompt')
-    console.print('[ y ] yes', style='prompt')
-    console.print('[ n ] no, i know what to do already', style='prompt')
+    console.print('Would you like to view the tutorial?', style='prompt')
+    console.print('[ y ] yes')
+    console.print('[ n ] no, i know what to do already')
     inpt = console.input('\n> ')
 
     if inpt == 'y':
         console.print('\nEnter the names of classes in the following format: class | letter grade | weight')
-        console.print('The class name can be anything 24 characters or under', highlight=False)
+        console.print('The class name can be anything 24 characters or under and cannot be used twice')
         console.print('The letter grade is your letter grade in the class (pluses and minuses are supported, but a+ is not)')
-        console.print('The weight can be entered in as either \'r\' (regular), \'p\' (partial), or \'f\' (full)', highlight=False)
+        console.print('The weight can be entered in as either \'r\' (regular), \'p\' (partial), or \'f\' (full)')
         console.print('Example: computer science | a- | f')
-        console.print('At any time, enter \'done\' to finish and save the classes', highlight=False)
+        console.print('At any time, enter \'done\' to finish and save the classes')
         console.input('\npress [ enter ] to continue > ')
         create()
     elif inpt == 'n':
@@ -236,8 +204,8 @@ def view():
 
         console.print(table)
 
-        console.print(f'Cumulative Weighted GPA: {calculateGPA("w")}')
-        console.print(f'Cumulative Unweighted GPA: {calculateGPA("u")}')
+        console.print(f'Cumulative Weighted GPA: [prompt]{calculateGPA("w")}[/prompt]')
+        console.print(f'Cumulative Unweighted GPA: [prompt]{calculateGPA("u")}[/prompt]')
     else:
         console.print('Record is empty. Try adding classes to it by entering [ n ] in the menu.', style='error')
 
@@ -248,35 +216,45 @@ def edit():
 
     hr()
     console.print('What class would you like to edit/ delete (note that this is case-sensitive)? (enter [ x ] to cancel)', style='prompt')
-    inpt = console.input('> ')
+    edit_class = console.input('> ')
 
-    cnt = 0
-    for el in record:
-        if el['class'] == inpt:
-            cnt += 1
-
-    if cnt > 0:
-        console.print(f'\nWould value of {inpt} would you like to edit?\n', style='prompt')
-        console.print('[ 1 ] class name', style='prompt')
-        console.print('[ 2 ] letter grade', style='prompt')
-        console.print('[ 3 ] class weight', style='prompt')
-        console.print('[ 4 ] delete class', style='prompt')
+    if edit_class == 'x':
+        menu()
+    elif edit_class in record_classes():
+        console.print(f'\nWhat value of {edit_class} would you like to edit?', style='prompt')
+        console.print('[ 1 ] class name')
+        console.print('[ 2 ] letter grade')
+        console.print('[ 3 ] class weight')
+        console.print('[ 4 ] delete class')
+        console.print('[ x ] cancel')
         inpt = console.input('> ')
 
         if inpt == '1':
-            if cnt > 1:
-                console.print('\nThere are multiple occurances of the specified class. Would you like to edit all of them or just the first occurance?\n', style='prompt')
-                console.print('[ 1 ] edit only the first occurance', style='prompt')
-                console.print('[ 2 ] edit all occurances', style='prompt')
-                inpt = console.input('\n> ')
+            console.print(f'\nWhat would you like to change the name of {edit_class} to?', style='prompt')
+            new = console.input('> ')
 
-                if inpt == '1':
-                    console.print('\nWhat would you like to change')
-            else:
-                idx = record.index(inpt)
-                record[idx] == inpt
-    elif inpt == 'x':
-        menu()
+            existing_classes = record_classes().remove(edit_class)
+
+            if new and len(new) <= 24 and new not in existing_classes:
+                ls = record_classes()
+                idx = 0
+                itr = 0
+                while itr < len(ls):
+                    if ls[itr] == edit_class:
+                        idx = itr
+                    itr += 1
+
+                record[idx]['class'] = new
+
+                console.print('\nClass name successfully changed!', style='success')
+                console.input('press [ enter ] to continue > ')
+                menu()
+        elif inpt == 'x':
+            menu()
+        else:
+            console.print('\n[ERROR] Please enter a valid option.', style='error')
+            console.input('press [ enter ] to continue > ')
+            edit()
     else:
         console.print('\n[ERROR] No matching class name. Please try again.', style='error')
         console.input('\npress [ enter ] to continue > ')
@@ -284,16 +262,47 @@ def edit():
 
 def export():
     hr()
-    console.print('Would you like to convert your current record to a .csv (comma seperated values) file?', style='prompt')
-    console.print('Note that to make the most use out of this file, you should open it with a spreadsheet-viewing tool such as Google Sheets or Microsoft Excel.\n', style='prompt')
-
-    console.print('[ y ] yes', style='prompt')
-    console.print('[ n ] no', style='prompt')
+    console.print("Would you like to convert your current record to a Excel (.xlsx) file?", style='prompt')
+    console.print('[ y ] yes')
+    console.print('[ n ] no')
 
     inpt = console.input('\n> ')
 
     if inpt == 'y' and len(record) > 0:
-        pass
+        files = listdir()
+        if 'records' not in files:
+            mkdir('records')
+
+        i = 1
+        while exists(f'gpacalc{i}.xlsx'):
+            i += 1
+
+        file_name = f'gpacalc{i}.xlsx'
+
+        workbook = Workbook()
+        worksheet = workbook.active
+        worksheet.title = 'gpacalc'
+        worksheet.sheet_properties.tabColor = "5993f0"
+
+        worksheet.cell(row=1, column=1, value='CLASS')
+        worksheet.cell(row=1, column=2, value='GRADE')
+        worksheet.cell(row=1, column=3, value='WEIGHT')
+        worksheet.cell(row=1, column=4, value='GPA')
+
+        itr = 0
+        while itr < len(record):
+            worksheet.cell(row=itr+2, column=1, value=record[itr]['class'])
+            worksheet.cell(row=itr+2, column=2, value=record[itr]['grade'])
+            worksheet.cell(row=itr+2, column=3, value=record[itr]['weight'])
+            worksheet.cell(row=itr+2, column=4, value=record[itr]['gpa'])
+            itr += 1
+
+        workbook.save(filename=f'records/{file_name}')
+
+        console.print(f"\nSpreadsheet titled '{file_name}' successfully generated in the records folder!", style='success')
+        console.print('To open this file, you would need an external tool such as Google Sheets or Microsoft Excel.')
+        console.input('press [ enter ] to continue > ')
+        menu()
     elif inpt == 'y' and len(record) == 0:
         console.print('\n[ERROR] You must have at least one element in your record to export it as .csv', style='error')
         console.input('press [ enter ] to continue > ')
@@ -302,5 +311,44 @@ def export():
     else:
         console.print('\n[ERROR] Please enter a valid option.', style='error')
         console.input('press [ enter ] to continue > ')
+
+def calculateGPA(weight):
+    addGPA = 0
+    if weight == 'w': # weighted
+        for el in record:
+            addGPA += el['gpa']
+    elif weight == 'u': # unweighted
+        for el in record:
+            if el['grade'] == 'a':
+                addGPA += 4.0
+            elif el['grade'] == 'a-':
+                addGPA += 3.667
+            elif el['grade'] == 'b+':
+                addGPA += 3.333
+            elif el['grade'] == 'b':
+                addGPA += 3.0
+            elif el['grade'] == 'b-':
+                addGPA += 2.667
+            elif el['grade'] == 'c+':
+                addGPA += 2.333
+            elif el['grade'] == 'c':
+                addGPA += 2.0
+            elif el['grade'] == 'c-':
+                addGPA += 1.667
+            elif el['grade'] == 'd+':
+                addGPA += 1.333
+            elif el['grade'] == 'd':
+                addGPA += 1.0
+            elif el['grade'] == 'd-':
+                addGPA += 0.667
+            elif el['grade'] == 'f':
+                addGPA += 0.0
+    return round(addGPA / len(record), 3)
+    
+def record_classes():
+    class_names = []
+    for el in record:
+        class_names.append(f'{el["class"]}')
+    return class_names
 
 init()
